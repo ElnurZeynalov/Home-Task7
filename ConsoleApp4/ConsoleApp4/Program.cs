2404111b673901b7a7f43813105c9f64b303f1a0
ElnurZeynalov <tu74449ce@code.edu.az>
2022-03-15 10:09:39 +0400

    Task 1 (I believe it is not so)

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Metini taxil edin: ");
+            String Sentence = Console.ReadLine();
+            SpaceDeleter(Sentence);
+        }
+        static void SpaceDeleter(string Sentence)
+        {
+            int First = Sentence.IndexOf(' ');
+            int Last = Sentence.LastIndexOf(' ');
+            int value = Last - First;
+            Console.WriteLine(Sentence.Remove(First, value));
+        }
+    }
+}
