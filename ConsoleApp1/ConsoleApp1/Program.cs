97b828fd59813d5829c377cca9399beb6c7c0654
ElnurZeynalov <tu74449ce@code.edu.az>
2022-03-15 10:08:01 +0400

    Task 2 Complete

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Sozu daxil edin(En az 1 boyuk 1 kicik herf ve reqem olsun)");
+            string value;
+            do
+            {
+                value = Console.ReadLine();
+                Console.WriteLine(Checker(value)); 
+            } while (!Checker(value)); 
+        }
+        static bool Checker(string value)
+        {
+            int count1 = 0;
+            int count2 = 0;
+            int count3= 0;
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (char.IsUpper(value[i]))
+                {
+                    count1++;
+                }
+                else if (char.IsLower(value[i]))
+                {
+                    count2++;
+                }
+                else if (char.IsDigit(value[i]))
+                {
+                    count3++;
+                }
+            }
+            if (count1 >= 1 && count2 >= 1 && count3 >= 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
