f5c308312a67ad3799c1324e4016e7e0f666ce0c
ElnurZeynalov <tu74449ce@code.edu.az>
2022-03-15 10:08:13 +0400

    Task 4 Complete

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine("Arrayin elementleri");
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            IntResize(ref array);
+            
+        }
+       static void IntResize(ref int[] array)
+        {
+            Console.WriteLine("Arraya Elave olunacaq Reqemi daxil edin");
+            string Element;
+            bool result;
+            int Value;
+            do
+            {
+                Element = Console.ReadLine();
+                result = int.TryParse(Element, out Value);
+                if (!result)
+                Console.WriteLine("Reqem daxil edin");
+            } while (!result);
+            Value = Convert.ToInt32(Element);
+            Array.Resize(ref array, array.Length+1);
+            array[array.Length-1]=Value;
+            Console.WriteLine("Yeni Array");
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
